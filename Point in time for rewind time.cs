add3fb3699c84eb8f99f45834ce0595c037818c7
pietras333 <83142069+pietras333@users.noreply.github.com>
2021-06-21 16:32:04 +0200

    Create Point in time for rewind time.cs

---
+++ Point in time for rewind time.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointInTime : MonoBehaviour
+{
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public PointInTime (Vector3 _position, Quaternion _rotation)
+    {
+        position = _position;
+        rotation = _rotation;
+    }
+}
