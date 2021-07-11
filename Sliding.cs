aba10b739767fb7dcd88646e1924bae4831d2d27
pietras333 <83142069+pietras333@users.noreply.github.com>
2021-07-11 17:19:57 +0200

    Update Sliding.cs

--- Sliding.cs
+++ Sliding.cs
@@ -4,33 +4,31 @@ using UnityEngine;
 
 public class Slide : MonoBehaviour
 {
-  // this is only for RigidBody based movement that's using movementMultilpier float to move
-  float slideScale
-
+ // this is only for RigidBody based movement that's using movementMultilpier float to move
  void Update(){
-  slideForce = 2 * rb.velocity.magnitude;
+ slideForce = 2 * rb.velocity.magnitude;
   
-  if(isGrounded && Input.GetKeyDown(KeyCode.C)){
-    StartSliding();
-  }
-  if(isSliding && rb.velocity.magnitude < 2f || Input.GetKeyUp(KeyCode.C)){
-    StopSliding();
-  }
+ if(isGrounded && Input.GetKeyDown(KeyCode.C)){
+   StartSliding();
+ }
+ if(isSliding && rb.velocity.magnitude < 2f || Input.GetKeyUp(KeyCode.C)){
+   StopSliding();
+ }
 }
  void StartSliding(){
-      isSliding = true;
-      movementMultiplier = 0f;
-      Vector3 slideScale = new Vector3(1f,0.5f,1f);
-      gameObject.transform.localScale = slideScale;
-      rb.AddForce(cameraTransform.forward * slideForce, ForceMode.Impulse);
-      rb.AddForce(cameraTransform.forward * -5f, ForceMode.Impulse);
-    }
+     isSliding = true;
+     movementMultiplier = 0f;
+     Vector3 slideScale = new Vector3(1f,0.5f,1f);
+     gameObject.transform.localScale = slideScale;
+     rb.AddForce(cameraTransform.forward * slideForce, ForceMode.Impulse);
+     rb.AddForce(cameraTransform.forward * -5f, ForceMode.Impulse);
+ }
 
-  void StopSliding(){
-      isSliding = false;
-      movementMultiplier = 10f;
-      Vector3 normalScale = new Vector3(1f,1f,1f);
-      gameObject.transform.localScale = normalScale;
-  } 
+ void StopSliding(){
+     isSliding = false;
+     movementMultiplier = 10f;
+     Vector3 normalScale = new Vector3(1f,1f,1f);
+     gameObject.transform.localScale = normalScale;
+ } 
    
 }
